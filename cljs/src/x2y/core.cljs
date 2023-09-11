41ed391fdcbd63d62b137bafccf3d5d2d4669088
pimgeek <pimgeek@outlook.com>
2023-09-12 00:19:55 +0800

    说明：增加或修改以下文件
    1 cljs/src/x2y/core.cljs:
      增加以下函数
        get-nth-column 提取给定 TSV 文件的某一列，返回字符串
        get-nth-column-vec 提取给定 TSV 文件的某一列，返回向量
        single-non-ascii? 判断给定字符是否为单个非 ASCII 字符
        count-single-non-ascii-lines 统计某列文本中非 ASCII 字符的数量
        count-element-dups 统计某列文本中每个字符串重复出现的次数，返回 map
      修改以下函数
        jsCodemapStats 增加统计项：单字数，重码数
    2 model/ime_tools.json: 修改输入输出范例文本

--- cljs/src/x2y/core.cljs
+++ cljs/src/x2y/core.cljs
@@ -15,10 +15,49 @@
        text2 (str/replace text1 #"(?iu)^[\r\n]+" "")]
     text2))
 
+;; 统计文本行数
 (defn wc [text]
   (let
       [lines (str/split-lines text)]
     (count lines)))
+
+;; 判断给定字符是否为单个非 ASCII 字符
+(defn single-non-ascii? [s]
+  (let
+      [matched (re-seq #"(?u)^[^\u0000-\u007f]$" s)]
+    (if matched true false)))
+
+;;
+(defn get-nth-column [tsv n]
+  (let
+      [lines (str/split-lines tsv)
+       vectors (map (fn [s] (str/split s "\t")) lines)
+       nth-of-lines (map (fn [l] (nth l (- n 1))) vectors)]
+    (clojure.string/join "\n" nth-of-lines)))
+
+;;
+(defn get-nth-column-vec [tsv n]
+  (let
+      [lines (str/split-lines tsv)
+       vectors (map (fn [s] (str/split s "\t")) lines)
+       nth-of-lines (map (fn [l] (nth l (- n 1))) vectors)]
+    nth-of-lines))
+
+;; 统计给定码表中的单字数 (码表格式：字词\t编码\t频率)
+(defn count-single-non-ascii-lines [lines]
+  (let
+      [sc-lines (filter single-non-ascii? lines)]
+    (count sc-lines)))
+
+;; 对给定的字符串列表分类汇总，统计每个字符串重复出现的次数
+;; 返回一个哈希表，便于事后查询
+(defn count-element-dups [v]
+  (let
+      [sorted (sort v)
+       grouped (partition-by identity sorted)
+       subtotal (map count grouped)
+       counted (map (fn [s p] {(first s) p}) grouped subtotal)]
+    (reduce merge counted)))
 
 ;;-- 对外输出的函数定义 --
 
@@ -30,8 +69,18 @@
 (defn ^:export jsLower2Upper [notes]
   (str/upper-case notes))
 
-;;
+;; 输出码表文件的统计分析信息
 (defn ^:export jsCodemapStats [codemap]
   (let
-      [line-count (wc codemap)]
-    (str "总行数：" (str line-count))))
+      [line-count (wc codemap)
+       first-col (get-nth-column-vec codemap 1)
+       first-col-uniq (distinct first-col)
+       second-col (get-nth-column-vec codemap 2)
+       second-col-uniq (distinct second-col)
+       sna-lines (filter single-non-ascii? first-col-uniq)
+       sna-count (count sna-lines)
+       dups-in-second-col (- (count second-col)
+                             (count second-col-uniq))]
+    (str "总行数：" (str line-count) "\n"
+         "单字数：" (str sna-count) "\n"
+         "重码数：" (str dups-in-second-col) "\n")))
